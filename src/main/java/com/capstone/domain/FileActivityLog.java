eb168b184b9b0ea84c90576cc034af8b39502f06
beginerer <newstart1379@naver.com>
2025-05-08 17:44:52 +0900

    chore : 로그 테이블 상태 필드 추가및 상태변경 메서드 구현

--- src/main/java/com/capstone/domain/FileActivityLog.java
+++ src/main/java/com/capstone/domain/FileActivityLog.java
@@ -28,6 +28,10 @@ public class FileActivityLog {
 
     @Enumerated(value = EnumType.STRING)
     @Column(nullable = false)
+    private FileStatus status;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
     private FileType fileType;
 
     @Column(nullable = false)
@@ -39,19 +43,29 @@ public class FileActivityLog {
     @Column(nullable = false)
     private LocalDateTime timeStamp;
 
+    @Column
+    private String errorMessage;
+
     @PrePersist
     protected void OnCreate() {
         timeStamp = LocalDateTime.now();
     }
 
-    FileActivityLog of(User user,ActionType actionType,FileType fileType,String filePath,String fileName) {
+    public static FileActivityLog of(User user, ActionType actionType, FileType fileType, FileStatus status,
+                                     String filePath, String fileName) {
         FileActivityLog fileActivityLog = new FileActivityLog();
         fileActivityLog.user = user;
         fileActivityLog.actionType = actionType;
         fileActivityLog.fileType = fileType;
+        fileActivityLog.status = status;
         fileActivityLog.filePath = filePath;
         fileActivityLog.fileName = fileName;
         return fileActivityLog;
+    }
+
+    public void markAsFailed(String message) {
+        this.status = FileStatus.FAILURE;
+        this.errorMessage = message;
     }
 
 }
